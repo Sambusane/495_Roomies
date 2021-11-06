0da71df4cf30e7da33250b7e93874466d5b22dd6
Samuel Busane <sbusane@csumb.edu>
2021-11-05 21:47:15 -0700

    Added the sign up functionality and added alerts that show account successfully created and that displays any errors such as account exists

--- Roomies/Roomies/ViewController.swift
+++ Roomies/Roomies/ViewController.swift
@@ -6,9 +6,43 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onLogin(_ sender: Any) {
+        
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = self.usernameField.text
+        user.password = self.passwordField.text
+        
+        user.signUpInBackground {(succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                self.displayAlert(withTitle: "Error", message: error.localizedDescription)
+            } else {
+                self.displayAlert(withTitle: "Success", message: "Account has been successfully created")
+            }
+        }
+    
+    }
+    func displayAlert(withTitle title: String, message: String ){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if title == "Success"{
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+                alert.dismiss(animated: true)
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }))
+        }else{
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+        }
+        self.present(alert, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
