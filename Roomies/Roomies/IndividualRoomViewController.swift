8ffcd605dc38f6c7382b9d5634a47d8aca9e24b8
Cooper Steiner <csteiner@csumb.edu>
2021-11-29 20:07:09 -0800

    Styling
    Styling

--- Roomies/Roomies/IndividualRoomViewController.swift
+++ Roomies/Roomies/IndividualRoomViewController.swift
@@ -95,10 +95,23 @@ class IndividualRoomViewController: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+<<<<<<< Updated upstream
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
+=======
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//
+//
+//        let roomDetaiViewCOntroller = segue.destination as! AddUserViewController
+//        roomDetaiViewCOntroller.room = roomPass
+//
+//    }
+    
+>>>>>>> Stashed changes
 
 }
